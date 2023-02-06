0a1a97e22a3b2ab119518c6366f68518447351c9
Ronaldo Feitosa <ronaldo.feitosa@mercadolivre.com>
2023-02-06 15:29:36 -0300

    bulma and design

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,17 @@
 <template>
   <div>
-    <RouterLink to="/">Notes</RouterLink> | 
-    <RouterLink to="/stats">Stats</RouterLink>
-
-    <RouterView />
+    <NavBar />
+    <div class="container is-max-desktop px-2 py-4">
+      <RouterView />
+    </div>
   </div>
 </template>
+
+<script setup>
+  // imports
+  import NavBar from '@/components/Layout/NavBar.vue'
+</script>
+
+<style>
+  @import 'bulma/css/bulma.min.css';
+</style>
--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -1,5 +1,32 @@
 <template>
 	<div class="notes">
-		<h1>Notes</h1>
+
+		<div class="card has-background-success-dark p-4 mb-5">
+			<div class="field">
+				<div class="control">
+					<textarea class="textarea" placeholder="Escreva uma nota" />
+				</div>
+			</div>
+
+			<div class="field is-grouped is-grouped-right">
+				<div class="control">
+					<button class="button is-link has-background-success">
+						Adicionar Nota
+					</button>
+				</div>				
+			</div>
+		</div>
+
+		<div v-for="i in 3" class="card mb-4">
+			<div class="card-content">
+				<div class="content">
+					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos soluta, molestias non odio expedita deserunt eius commodi distinctio at voluptatem facilis, quibusdam quaerat excepturi perferendis quia totam! Voluptate, labore fuga!
+				</div>
+			</div>
+			<footer class="card-footer">
+				<a href="#" class="card-footer-item">Edit</a>
+				<a href="#" class="card-footer-item">Delete</a>
+			</footer>
+		</div>
 	</div>
 </template>
